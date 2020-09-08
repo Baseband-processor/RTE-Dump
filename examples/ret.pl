f6679e9c08b62eeee379bd292d9ef9d068ed0202
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-09-08 14:35:18 +0200

    Update ret.pl

--- examples/ret.pl
+++ examples/ret.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 
+sub BEGIN{
+
 no strict;
+no warnings;
 use RTE::Dump qw(:dump_hex);
 
 my $file = 'dumper.txt';
@@ -8,3 +11,5 @@ my $file = 'dumper.txt';
 open(FILE, '>', $file);
 
 print RTE::Dump::rte_hexdump(\FILE, "AAAAAA", "0x999999999", 22000);
+
+}
